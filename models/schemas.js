778db7a7c07bd76543bcda7f8f586db8422f15db
Abhinavjain123 <abhinavsra@gmail.com>
2023-09-25 11:50:51 +0530

    Added schemas

---
+++ models/schemas.js
@@ -0,0 +1,44 @@
+
+
+// Creating a mongoose collection Schema
+const petOwner = new mongoose.Schema({
+    uname: String,
+    password: String,
+    fname: String,
+    lname: String,
+    email: String,
+    phno: Number,
+    Vaccine_status: Boolean,
+    img_url: String,
+    address: String,
+    address2: String,
+    city: String,
+    state: String,
+    zip: Number
+});
+
+const petCareProvider = new mongoose.Schema({
+    uname: String,
+    password: String,
+    name: String,
+    email: String,
+    org: String,
+    phno: Number,
+    phno2: Number,
+    address: String,
+    address2: String,
+    city: String,
+    state: String,
+    zip: Number
+});
+
+const booking = new mongoose.Schema({
+    uname: String,
+    provider_name: String,
+    schedule: Date,
+    agenda: String
+});
+
+
+
+
